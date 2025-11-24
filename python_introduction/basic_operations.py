0d48ad8faaf5538aeab7a82eacfecfc4e15f5363
Ntsana Pheko <ntsanapheko@gmail.com>
2025-11-24 17:02:15 +0200

    basic_operations

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,10 @@
+number1 = 10 
+number2 = 5
+
+sum = number1 + number2
+difference = number1 - number2 
+product = number1 * number2 
+
+print("Addition of 10 and 5 is " + str(sum))
+print("Subtraction of 10 and 5 is " + str(difference))
+print("Multiplication of 10 and 5 is " + str(product))
